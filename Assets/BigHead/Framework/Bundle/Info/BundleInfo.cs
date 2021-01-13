2a3636fe7306941d0464c26df9318da5f8fd858e
liyang <xuelang0319@163.com>
2021-01-13 20:45:40 +0800

    [NEW] 添加Bundle配置类

---
+++ Assets/BigHead/Framework/Bundle/Info/BundleInfo.cs
@@ -0,0 +1,24 @@
+﻿//
+// = The script is part of BigHead and the framework is individually owned by Eric Lee.
+// = Cannot be commercially used without the authorization.
+//
+//  Author  |  UpdateTime     |   Desc  
+//  Eric    |  2021年1月13日   |   包信息
+//
+
+using System;
+
+namespace BigHead.Framework.Bundle.Info
+{
+    [Serializable]
+    public class BundleInfo
+    {
+        public string Path { get; set; }
+
+        public string Name { get; set; }
+
+        public int Version { get; set; }
+
+        public string CheckCode { get; set; }
+    }
+}
---
+++ Assets/BigHead/Framework/Bundle/Info/BundleTable.cs
@@ -0,0 +1,19 @@
+﻿//
+// = The script is part of BigHead and the framework is individually owned by Eric Lee.
+// = Cannot be commercially used without the authorization.
+//
+//  Author  |  UpdateTime     |   Desc  
+//  Eric    |  2021年1月13日   |   打包汇总表
+//
+
+using System;
+using System.Collections.Generic;
+
+namespace BigHead.Framework.Bundle.Info
+{
+    [Serializable]
+    public class BundleTable
+    {
+        public List<BundleInfo> BundleInfos { get; set; }
+    }
+}
