dec099dfeccd0a9298e07824b1712e68e3c47bd1
liyang <xuelang0319@163.com>
2021-01-14 16:53:07 +0800

    [NEW] 新增文件读取器

---
+++ Assets/BigHead/Framework/Utility/Readers/FileInfoReader.cs
@@ -0,0 +1,36 @@
+﻿//
+// = The script is part of BigHead and the framework is individually owned by Eric Lee.
+// = Cannot be commercially used without the authorization.
+//
+//  Author  |  UpdateTime     |   Desc  
+//  Eric    |  2021年1月14日   |   文件读取器
+//
+
+using System;
+using System.IO;
+using System.Text;
+using BigHead.Framework.Extension;
+
+namespace BigHead.Framework.Utility.Readers
+{
+    public static class FileInfoReader
+    {
+        public static string ReadFile(this FileInfo info)
+        {
+            try
+            {
+                using (var stream = info.OpenRead())
+                {
+                    byte[] buffer = new byte[stream.Length];
+                    int length = stream.Read(buffer, 0, buffer.Length);
+                    return Encoding.UTF8.GetString(buffer, 0, length);
+                }
+            }
+            catch (Exception e)
+            {
+                e.Exception();
+                return String.Empty;
+            }
+        }
+    }
+}
