512902b3a4b4bf9ffdd91136ba272c6c33922f5f
liyang <xuelang0319@163.com>
2021-05-23 23:08:35 +0800

    [TUN] 修复了单例的BUG，并添加区分为场景单例和全局单例

---
+++ Assets/BigHead/Framework/Core/MonoGlobalSingleton.cs
@@ -0,0 +1,31 @@
+﻿//
+// = The script is part of BigHead and the framework is individually owned by Eric Lee.
+// = Cannot be commercially used without the authorization.
+//
+//  Author  |  UpdateTime     |   Desc  
+//  Eric    |  2020年12月17日  |   Mono全局单例， 由于Unity以单线程运行，所以无需进行线程锁或其他方式优化。
+//
+
+using UnityEngine;
+
+public abstract class MonoGlobalSingleton<T> : MonoBehaviour where T : MonoGlobalSingleton<T>
+{
+    private static T _instance;
+
+    private static GameObject _object;
+
+    public static T Instance
+    {
+        get
+        {
+            if (!_object)
+            {
+                _object = new GameObject($"Singleton{typeof(T).Name}");
+                DontDestroyOnLoad(_object);
+                _instance = _object.AddComponent<T>();
+            }
+
+            return _instance;
+        }
+    }
+}
---
+++ Assets/BigHead/Framework/Core/MonoSceneSingleton.cs
@@ -0,0 +1,30 @@
+﻿//
+// = The script is part of BigHead and the framework is individually owned by Eric Lee.
+// = Cannot be commercially used without the authorization.
+//
+//  Author  |  UpdateTime     |   Desc  
+//  Eric    |  2021年5月23日   |   场景Mono单例
+//
+
+using UnityEngine;
+
+public class MonoSceneSingleton<T> : MonoBehaviour where T : MonoSceneSingleton<T>
+{
+    private static T _instance;
+
+    private static GameObject _object;
+
+    public static T Instance
+    {
+        get
+        {
+            if (!_object)
+            {
+                _object = new GameObject($"Singleton{typeof(T).Name}");
+                _instance = _object.AddComponent<T>();
+            }
+
+            return _instance;
+        }
+    }
+}
