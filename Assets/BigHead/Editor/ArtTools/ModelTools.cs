500d9bb9cb5da74d42bca0401ef308f3c9267934
liyang <xuelang0319@163.com>
2021-05-30 20:00:56 +0800

    [NEW] 添加美术检查工具，添加快捷键提示

---
+++ Assets/BigHead/Editor/ArtTools/ModelTools.cs
@@ -0,0 +1,52 @@
+﻿//
+// = The script is part of BigHead and the framework is individually owned by Eric Lee.
+// = Cannot be commercially used without the authorization.
+//
+//  Author  |  UpdateTime     |   Desc  
+//  Eric    |  2021年5月30日   |   模型工具
+//
+
+using BigHead.Framework.Core;
+using UnityEditor;
+using UnityEngine;
+
+namespace BigHead.Editor.ArtTools
+{
+    public static class ModelTools
+    {
+        /// <summary>
+        /// 获取模型的顶点数量和三角面数量
+        /// </summary>
+        public static void GetSelectionModelVertexesAndTriangularFacet()
+        {
+            var selections = Selection.gameObjects;
+            if (selections.Length.Equals(0))
+            {
+                "请至少在场景中选择一个GameObject!".Error();
+                return;
+            }
+
+            for (int i = 0; i < selections.Length; i++)
+            {
+                var item = selections[i];
+                var filters = item.GetComponentsInChildren<MeshFilter>();
+                if (filters.Length.Equals(0))
+                {
+                    $"当前选中的模型：{item.name} 中没有找到MeshFilter组件，请检查。".Error();
+                    continue;
+                }
+
+                var vertex = 0;
+                var triangular = 0;
+                for (int j = 0; j < filters.Length; j++)
+                {
+                    var mesh = filters[j].sharedMesh;
+                    vertex += mesh.vertexCount;
+                    triangular += mesh.triangles.Length / 3;
+                }
+                
+                $"当前选中的模型：{item.name} 中存在 {filters.Length} 个MeshFilter, 共计顶点数量为：{vertex}, 三角面数为：{triangular}".Highlight();
+            }
+        }
+    }
+}
--- Assets/BigHead/Editor/MenuItems.cs
+++ Assets/BigHead/Editor/MenuItems.cs
@@ -6,11 +6,23 @@
 //  Eric    |  2020年12月18日  |   编辑器菜单扩展项
 //
 
+using BigHead.Editor.ArtTools;
 using BigHead.Editor.Generate.GenCsv;
 using UnityEditor;
+using UnityEngine;
 
 namespace BigHead.Editor
 {
+    /*
+     * 添加快捷键：
+     * 单一建: _
+     * Shift: #
+     * Ctrl:  %
+     * Alt:   &
+     * 如: _t， #t,  %t, &t
+     * 可以添加如下快捷键： [MenuItem("BigHead/Test %t")], 即按Ctrl + t就可以直接触发该方法
+     */
+    
     public static class MenuItems
     {
         [MenuItem("BigHead/Test")]
@@ -23,6 +35,18 @@ namespace BigHead.Editor
         public static void ClearGenCsv()
         {
             Excel2Csv.ClearAll();
+        }
+        
+        [MenuItem("BigHead/Csv/Generate")]
+        public static void GenCsv()
+        {
+            Excel2Csv.Generate();
+        }
+
+        [MenuItem("BigHead/ArtTools/GetSelectionVertexes")]
+        public static void GetNumberOfVertexes()
+        {
+            ModelTools.GetSelectionModelVertexesAndTriangularFacet();
         }
     }
 }
