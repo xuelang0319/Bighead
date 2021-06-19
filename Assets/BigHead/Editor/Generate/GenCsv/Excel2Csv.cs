5faaaee384af1420cb025e56a2174662caff33ac
liyang <xuelang0319@163.com>
2021-06-19 22:45:29 +0800

    [FIX] 修复CSV生成错误。 [TUN] 优化了迭代过程中不必要的删除和创建。

--- Assets/BigHead/Editor/Generate/GenCsv/Excel2Csv.cs
+++ Assets/BigHead/Editor/Generate/GenCsv/Excel2Csv.cs
@@ -13,6 +13,8 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using BigHead.Framework.Core;
+using BigHead.Framework.Extension;
 using BigHead.Framework.Utility.Crypto;
 using BigHead.Framework.Utility.Helper;
 using Excel;
@@ -27,7 +29,6 @@ namespace BigHead.Editor.Generate.GenCsv
         {
             try
             {
-                DeleteDynamicCsv();
                 AnalysisExcel();
                 AssetDatabase.Refresh();
                 GC.Collect();
@@ -35,7 +36,7 @@ namespace BigHead.Editor.Generate.GenCsv
             catch (Exception e)
             {  
                 EditorUtility.ClearProgressBar();
-                Console.WriteLine(e);
+                e.Exception();
                 ClearAll();
             }
         }
@@ -67,112 +68,8 @@ namespace BigHead.Editor.Generate.GenCsv
             var resourcesCsvPath = ResourcesCsvPath;
             DirectoryHelper.ForceCreateDirectory(resourcesCsvPath);
 
-            // 获取Excel文件夹下所有文件路径
-            var files = Directory.GetFiles(excelPath, "*", SearchOption.AllDirectories);
-            var paths = files.Where(name => name.EndsWith(".xlsx") || name.EndsWith(".xls"))
-                .Select(name => name.Replace('/', '\\')).ToArray();
-
-            // 校验数据
-            var md5List = new List<string>();
-            
-            
-            // 正则比对名称，如果有括号则选用括号内的名称
-            var regex1 = new Regex(@"\((\w+)\)");
-            var regex2 = new Regex(@"\（(\w+)\）");
-            
-            float progress = -1;
-            foreach (var path in paths)
-            {
-                ++progress;
-                EditorUtility.DisplayProgressBar("解析Excel文件", path, progress / paths.Length);
-                using(var stream = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read))
-                {
-                    using(var excelReader = path.EndsWith(".xls")
-                        ? ExcelReaderFactory.CreateBinaryReader(stream)
-                        : ExcelReaderFactory.CreateOpenXmlReader(stream))
-                    {
-                        var dataSet = excelReader.AsDataSet();
-
-                        foreach (DataTable table in dataSet.Tables)
-                        {
-                            // 过滤不生成的表单
-                            var tableName = table.TableName.Trim();
-                            if (tableName.StartsWith("#"))
-                                continue;
-
-                            var rows = table.Rows;
-
-                            // 过滤不生成的列
-                            var ungenCols = new List<int>();
-                            for (int index = 0; index < table.Columns.Count; index++)
-                            {
-                                var value = rows[2][index].ToString().Trim();
-                                if (value == "不生成" || value.StartsWith("#") || string.IsNullOrEmpty(value))
-                                    ungenCols.Add(index);
-                            }
-
-                            var tableBuilder = new StringBuilder();
-                            for (int row = 0; row < table.Rows.Count; row++)
-                            {
-                                var rowBuilder = new StringBuilder();
-                                for (int col = 0; col < table.Columns.Count; col++)
-                                {
-                                    if (ungenCols.Contains(col))
-                                        continue;
-
-                                    var grid = rows[row][col].ToString()
-                                        .Replace('\n', '|')
-                                        .Replace(",", "、");
-                                    
-                                    rowBuilder.Append(grid);
-                                    
-                                    var builder = col != table.Columns.Count - 1
-                                        ? rowBuilder.Append(",") : 
-                                        row == table.Rows.Count - 1
-                                            ? rowBuilder.Append("")  
-                                            : rowBuilder.Append(Environment.NewLine);
-
-                                }
-                                
-                                var rowArray = rowBuilder.ToString().Split(',');
-                                if (!Array.TrueForAll(rowArray, string.IsNullOrEmpty) && 
-                                    !Array.TrueForAll(rowArray, string.IsNullOrWhiteSpace))
-                                {
-                                    tableBuilder.Append(rowBuilder);
-                                }
-
-                                if (regex1.IsMatch(tableName))
-                                    tableName = regex1.Match(tableName).Value.TrimStart('(').TrimEnd(')');
-
-                                if (regex2.IsMatch(tableName))
-                                    tableName = regex2.Match(tableName).Value.TrimStart('（').TrimEnd('）');
-                                
-                                if (row == 2)
-                                {
-                                    md5List.Add(tableName + "@" + BigHeadCrypto.MD5Encode(tableBuilder.ToString()));
-                                }
-                            }
-
-                            var str = tableBuilder.ToString().TrimEnd('\r', '\n');
-                            var data = Encoding.UTF8.GetBytes(str);
-                            using (var fileStream =
-                                new FileStream($"{dynamicCsvPath}/{tableName}.csv", FileMode.Create))
-                            {
-                                fileStream.Write(data, 0, data.Length);
-                            }
-                            
-                            if(GenerateCsvInResources)
-                                using (var fileStream =
-                                    new FileStream($"{resourcesCsvPath}/{tableName}.csv", FileMode.Create))
-                                {
-                                    fileStream.Write(data, 0, data.Length);
-                                }
-                        }
-                    }
-                }
-            }
-
-            var datas = new List<string>();
+            // 上一次生成存储的MD5数据
+            var oldDatas = new Dictionary<string, string>();
 
             // 读取生成存储信息
             var fullName = CsvConfigFullName;
@@ -183,40 +80,231 @@ namespace BigHead.Editor.Generate.GenCsv
                     var line = "";
                     while ((line = stream.ReadLine()) != null)
                     {
-                        datas.Add(line);
+                        var array = line.Split('@');
+                        // 0 - 文件名或全路径， 1 - 校验码
+                        oldDatas.Add(array[0], array[1]);
                     }
                 }
             }
 
-            var news = md5List.Except(datas).ToArray();
-            var olds = datas.Except(md5List).ToArray();
-
-            var count = 0f;
-            // 先删除旧的生成脚本
-            foreach (var item in olds)
+            // 获取Excel文件夹下所有文件路径
+            var files = Directory.GetFiles(excelPath, "*", SearchOption.AllDirectories);
+            var paths = files.Where(name => name.EndsWith(".xlsx") || name.EndsWith(".xls"))
+                .Select(name => name.Replace('/', '\\')).ToArray();
+            
+            // 发生变化的列表
+            var changedFilter = new List<string>();
+            // 此次生成存储的MD5数据
+            var newDatas = new Dictionary<string, string>();
+            
+            // 对每个Excel做MD5变更校验
+            foreach (var path in paths)
             {
-                var path = GenerateCsPath + item.Split('@')[0];
-                var rowPath = path + "Row.cs";
-                var csvPath = path + "Csv.cs";
-                FileHelper.DeleteFile(rowPath);
-                FileHelper.DeleteFile(csvPath);
+                var content = new FileInfo(path).ReadFile();
+                var md5 = BigHeadCrypto.MD5Encode(content);
+
+                var fileNameWithExtension = Path.GetFileName(path);
+                // 添加正确数据，直接修正，但对发生了变化或新增的文件进行记录
+                newDatas.Add(fileNameWithExtension, md5);
+
+                if (oldDatas.ContainsKey(fileNameWithExtension) &&
+                    Equals(oldDatas[fileNameWithExtension], md5))
+                {
+                    // 说明数据没有变化
+                    var excelName = Path.GetFileNameWithoutExtension(fileNameWithExtension);
+                    var all = oldDatas.Keys.Where(key =>
+                    {
+                        if (!key.StartsWith(excelName)) return false;
+                        return !string.IsNullOrEmpty(Path.GetExtension(key)) || 
+                               Equals(excelName.Split('$')[0], excelName);
+                    }).ToList();
+
+                    foreach (var key in all)
+                    {
+                        var value = oldDatas[key];
+                        newDatas.AddValue(key, value);
+                        oldDatas.Remove(key);
+                    }
+
+                    continue;
+                }
                 
-                ++count;
-                EditorUtility.DisplayProgressBar("正在删除旧的脚本文件", csvPath, count / olds.Length);
+                // 发生了变化
+                changedFilter.Add(path);
+                
+                // 由于即将进行处理，所以在这里移除。
+                if (oldDatas.ContainsKey(fileNameWithExtension))
+                    oldDatas.Remove(fileNameWithExtension);
             }
 
-            count = 0f;
-            // 增加新的生成脚本
-            foreach (var item in news)
+            // 正则比对名称，如果有括号则选用括号内的名称
+            var regex1 = new Regex(@"\((\w+)\)");
+            var regex2 = new Regex(@"\（(\w+)\）");
+            
+            // 遍历发生变更或新建的Excel
+            foreach (var path in changedFilter)
             {
-                var path = DynamicCsvPath + "/" + item.Split('@')[0] + ".csv";
-                Csv2Cs.GenerateCs(path);
-                
-                ++count;
-                EditorUtility.DisplayProgressBar("正在生成新的脚本文件", path, count / news.Length);
+                var excelName = Path.GetFileNameWithoutExtension(path);
+                using(var stream = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    using(var excelReader = path.EndsWith(".xls")
+                        ? ExcelReaderFactory.CreateBinaryReader(stream)
+                        : ExcelReaderFactory.CreateOpenXmlReader(stream))
+                    {
+                        var dataSet = excelReader.AsDataSet();
+
+                        float createProgress = 0;
+                        var tableCount = dataSet.Tables.Count;
+                        foreach (DataTable table in dataSet.Tables)
+                        {
+                            ++createProgress;
+                            // 过滤不生成的表单
+                            var tableName = table.TableName.Trim();
+                            if (tableName.StartsWith("#"))
+                                continue;
+
+                            // 正则后的表单名称
+                            if (regex1.IsMatch(tableName))
+                                tableName = regex1.Match(tableName).Value.TrimStart('(').TrimEnd(')');
+
+                            if (regex2.IsMatch(tableName))
+                                tableName = regex2.Match(tableName).Value.TrimStart('（').TrimEnd('）');
+                            
+                            EditorUtility.DisplayProgressBar($"正在解析：{excelName}", tableName, createProgress / tableCount);
+
+                            var rows = table.Rows;
+                            var cols = table.Columns;
+
+                            // 过滤不生成的列
+                            var ungenCols = new List<int>();
+                            // lastColIndex是为了解决当数据后几列都不生成时导致生成的数据格式错误问题。
+                            var lastColIndex = 0;
+                            for (int index = 0; index < cols.Count; index++)
+                            {
+                                var value = rows[2][index].ToString().Trim();
+                                if (value == "不生成" || value.StartsWith("#") || string.IsNullOrEmpty(value))
+                                    ungenCols.Add(index);
+                                else lastColIndex = index;
+                            }
+
+                            var tableBuilder = new StringBuilder();
+                            for (var row = 0; row < rows.Count; row++)
+                            {
+                                var rowBuilder = new StringBuilder();
+                                for (var col = 0; col < cols.Count; col++)
+                                {
+                                    var isUngen = ungenCols.Contains(col);
+                                    
+                                    if (!isUngen)
+                                    {
+                                        var grid = rows[row][col].ToString()
+                                            .Replace('\n', '|')
+                                            .Replace(",", "、");
+                                            rowBuilder.Append(grid);
+                                            
+                                        if (col != lastColIndex)
+                                                rowBuilder.Append(",");
+                                    }
+
+                                    if (col == lastColIndex && row != rows.Count - 1)
+                                        rowBuilder.AppendLine();
+                                }
+                                
+                                var rowArray = rowBuilder.ToString().Split(',');
+                                if (!Array.TrueForAll(rowArray, string.IsNullOrEmpty) && 
+                                    !Array.TrueForAll(rowArray, string.IsNullOrWhiteSpace))
+                                {
+                                    tableBuilder.Append(rowBuilder);
+                                }
+                            }
+
+                            var str = tableBuilder.ToString().TrimEnd('\r', '\n');
+                            
+                            // 这里进行MD5校验
+                            var md5 = BigHeadCrypto.MD5Encode(str);
+                            var dataName = $"{excelName}${tableName}";
+                            newDatas.Add(dataName, md5);
+
+                            var bundleDynamicPath = $"{dynamicCsvPath}/{tableName}.csv";
+                            var resourceDynamicPath = $"{resourcesCsvPath}/{tableName}.csv";
+                            
+                            // 如果进入判断说明存在，否则为新增
+                            if (oldDatas.ContainsKey(dataName))
+                            {
+                                var value = oldDatas[dataName];
+                                // 移除旧数据存储，因为这里已经进行了整体处理。
+                                oldDatas.Remove(dataName);
+                                
+                                // 存在未改变
+                                if (Equals(value, md5)) continue;
+
+                                // 已改变，删除原来生成的数据。  -csv, -.cs
+                                EditorUtility.DisplayProgressBar($"删除变更前文件： {tableName}", path, createProgress / tableCount);
+                                File.Delete(bundleDynamicPath);
+                                var rowPath = GenerateCsPath + tableName + "Row.cs";
+                                var csvPath = GenerateCsPath + tableName + "Csv.cs";
+                                File.Delete(rowPath);
+                                File.Delete(csvPath);
+                            
+
+                                if (GenerateCsvInResources)
+                                    File.Delete(resourceDynamicPath);
+                            }
+
+                            var data = Encoding.UTF8.GetBytes(str);
+                            using (var fileStream = new FileStream(bundleDynamicPath, FileMode.Create))
+                            {
+                                fileStream.Write(data, 0, data.Length);
+                            }
+                            
+                            Csv2Cs.GenerateCs(bundleDynamicPath);
+                            
+                            if(GenerateCsvInResources)
+                            {
+                                using (var fileStream = new FileStream(resourceDynamicPath, FileMode.Create))
+                                {
+                                    fileStream.Write(data, 0, data.Length);
+                                }
+                            }
+                        }
+                    }
+                }
             }
+
+            var deleteProgress = 0;
+            var deleteCount = oldDatas.Count;
+            // 没有被移除的均是被删除的文件或Sheet，在这里要进行删除对应的Csv和.cs文件
+            foreach (var key in oldDatas.Keys)
+            {
+                ++deleteProgress;
+                EditorUtility.DisplayProgressBar(  $"正在删除旧数据:", key, deleteProgress / deleteCount);
+                var array = key.Split('$');
+                // 这是Excel文件，无需处理。
+                if(array.Length == 1) 
+                    continue;
+                
+                // 这是Csv文件。
+                var csvName = array[1];
+                
+                var bundleDynamicPath = $"{dynamicCsvPath}/{csvName}.csv";
+                File.Delete(bundleDynamicPath);
+                var rowPath = GenerateCsPath + csvName + "Row.cs";
+                var csvPath = GenerateCsPath + csvName + "Csv.cs";
+                File.Delete(rowPath);
+                File.Delete(csvPath);
+
+                if (GenerateCsvInResources)
+                {
+                    var resourceDynamicPath = $"{resourcesCsvPath}/{csvName}.csv";
+                    File.Delete(resourceDynamicPath);
+                }
+            }
+
+
+            var strs = newDatas.Select(kv => $"{kv.Key}@{kv.Value}").ToArray();
+            var info = string.Join(Environment.NewLine, strs);
             // 生成最新的配置文件
-            var bytes = Encoding.UTF8.GetBytes(string.Join(Environment.NewLine, md5List));
+            var bytes = Encoding.UTF8.GetBytes(info);
             var configFullName = CsvConfigFullName;
             if (File.Exists(configFullName))
                 File.Delete(configFullName);
