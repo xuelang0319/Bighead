68d36dc0479755612ea08ed1604e8f6b24af55a8
liyang <xuelang0319@163.com>
2021-06-26 01:05:54 +0800

    [NEW] 添加CsvAssistant初始化方法。

--- Assets/BigHead/Framework/Utility/GenCsv/CsvAssistant.cs
+++ Assets/BigHead/Framework/Utility/GenCsv/CsvAssistant.cs
@@ -7,7 +7,35 @@
 //
 
 
+using System;
+using System.Linq;
+using BigHead.Framework.Core;
+using BigHead.Framework.Utility.Helper;
+using UnityEngine;
+
 public static partial class CsvAssistant
 {
     public static T AsPlus<T>(this BasicCsv t) where T: BasicCsv => (T) t;
+
+    public static void Step(Action callback)
+    {
+        var array = typeof(BasicCsv).CreateAllDerivedClass<BasicCsv>().ToList();
+        var waitCount = array.Count;
+        if (waitCount == 0)
+        {
+            "没有检测到任何Csv配置表，请检查。".Exception();
+            callback?.Invoke();
+        }
+
+        foreach (var csv in array)
+        {
+            CsvFunctions.RegisterCsv(csv.GetType().Name, csv);
+            csv.InitCsv(() =>
+            {
+                --waitCount;
+                if(Equals(waitCount, 0)) 
+                    callback?.Invoke();
+            });
+        }
+    }
 }
