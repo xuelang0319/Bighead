811d4394dadd52790f29c0d4e8126681ea9eba09
liyang <xuelang0319@163.com>
2021-06-26 21:27:23 +0800

    [NEW] 增加全局监听事件

--- Assets/BigHead/Customer/EnumListener.cs
+++ Assets/BigHead/Customer/EnumListener.cs
@@ -8,5 +8,18 @@
 
 public enum EnumListener
 {
+    /// <summary> 语言变更事件 </summary>
     Language,
+    /// <summary> 音效变更事件 </summary>
+    SoundVolume,
+    /// <summary> 音乐变更事件 </summary>
+    MusicVolume,
+    /// <summary> 音效静音变更事件 </summary>
+    SoundMute,
+    /// <summary> 音乐静音变更事件 </summary>
+    MusicMute,
+    /// <summary> 暂停事件 </summary>
+    Pause,
+    /// <summary> 恢复事件 </summary>
+    Resume,
 }
