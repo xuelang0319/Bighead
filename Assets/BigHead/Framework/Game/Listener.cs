5caaf1fb9732358a7ca0be1c8c402d53cf4da9bd
liyang <liyang@dxx.cn>
2021-07-19 10:30:41 +0800

    [FIX] 修复监听广播过程中报错问题。

--- Assets/BigHead/Framework/Game/Listener.cs
+++ Assets/BigHead/Framework/Game/Listener.cs
@@ -4,6 +4,7 @@
 //
 //  Author  |  UpdateTime     |   Desc  
 //  Eric    |  2021年5月29日   |   观察者模式
+//  Eric    |  2021年7月18日   |   防止在广播过程中出现添加或移除某个监听发生问题。
 //
 
 using System;
@@ -14,40 +15,59 @@ namespace BigHead.Framework.Game
 {
     public class Listener : Singleton<Listener>
     {
-        private Dictionary<string, List<Action<object[]>>> _listeners = new Dictionary<string, List<Action<object[]>>>();
+        public class ListenerItem
+        {
+            public string Key;
+            public Action<object[]> Callback;
+        }
         
+        private Dictionary<string, List<Action<object[]>>> _listeners = new Dictionary<string, List<Action<object[]>>>();
+        private Queue<ListenerItem> _waitAdd = new Queue<ListenerItem>();
+        private Queue<ListenerItem> _waitRemove = new Queue<ListenerItem>();
+
         public void Broadcast(string key, params object[] message)
         {
+            if (_waitAdd.Count > 0)
+            {
+                while (_waitAdd.Count > 0)
+                {
+                    var item = _waitAdd.Dequeue();
+                    if (!_listeners.ContainsKey(item.Key))
+                        _listeners[item.Key] = new List<Action<object[]>>();
+            
+                    _listeners[item.Key].Add(item.Callback);
+                }
+            }
+
+            if (_waitRemove.Count > 0)
+            {
+                while (_waitRemove.Count > 0)
+                {
+                    var item = _waitRemove.Dequeue();
+                    if (!_listeners.ContainsKey(item.Key) || !_listeners[item.Key].Contains(item.Callback))
+                    {
+                        $"Listener can not find callback with key: {key}, please make sure you have register your callback before".Exception();
+                        return;
+                    }
+
+                    _listeners[item.Key].Remove(item.Callback);
+                    if (_listeners[item.Key].Count == 0)
+                        _listeners.Remove(item.Key);
+                }
+            }
+
             if (!_listeners.ContainsKey(key)) return;
             _listeners[key].ForEach(action => action?.Invoke(message));
         }
 
         public void Add(string key, Action<object[]> callback)
         {
-
-            if (!_listeners.ContainsKey(key))
-                _listeners[key] = new List<Action<object[]>>();
-            
-            _listeners[key].Add(callback);
+            _waitAdd.Enqueue(new ListenerItem {Key = key, Callback = callback});
         }
 
         public void Remove(string key, Action<object[]> callback)
         {
-            if (!_listeners.ContainsKey(key) || !_listeners[key].Contains(callback))
-            {
-                $"Listener can not find callback with key: {key}, please make sure you have register your callback before".Exception();
-                return;
-            }
-
-            _listeners[key].Remove(callback);
-            if (_listeners[key].Count == 0)
-                _listeners.Remove(key);
-        }
-
-        private void OnDestroy()
-        {
-            _listeners.Clear();
-            _listeners = null;
+            _waitRemove.Enqueue(new ListenerItem {Key = key, Callback = callback});
         }
     }
 }
