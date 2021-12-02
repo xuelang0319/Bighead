412c79a69a7c976771e63eb8ccab438ea553e0a6
Eric.Lee <110925095@qq.com>
2021-12-02 19:54:16 +0800

    [NEW] 增加Task及TaskManager托管功能。

---
+++ Assets/BigHead/Framework/Game/Tasks/TaskQueue.cs
@@ -0,0 +1,41 @@
+﻿//
+// = The script is part of Bighead and the framework is individually owned by Eric Lee.
+// = Cannot be commercially used without the authorization.
+//
+//  Author  |  UpdateTime     |   Desc  
+//  Eric    |  2021年12月02日  |   Task队列执行
+//
+
+using System.Collections.Generic;
+using BigHead.Framework.Core;
+
+namespace BigHead.Framework.Game.Tasks
+{
+    public class TaskQueue : TaskBase
+    {
+        private readonly Queue<TaskBase> _queue = new Queue<TaskBase>();
+
+        public void Add(TaskBase task)
+        {
+            _queue.Enqueue(task);
+        }
+
+        public override TaskState Update(float deltaTime)
+        {
+            var state = _queue.Peek().Update(deltaTime);
+            switch (state)
+            {
+                case TaskState.Running: 
+                    return TaskState.Running;
+                case TaskState.Success:
+                    _queue.Dequeue();
+                    return _queue.Count > 0 ? TaskState.Running : TaskState.Success;
+                case TaskState.Break:
+                    return TaskState.Success;
+                default:
+                    $"Task state {state} error.".Error();
+                    return TaskState.Success;
+            }
+        }
+    }
+}
